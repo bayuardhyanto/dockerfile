1157e37f1c396140c52cbde25ade2f9f7d8e86e2
bayuardhyanto <bayu@celepuk.com>
2023-11-04 21:20:50 +0700

    update

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
 docker build -t dockercelepuk/from from --progress=plain
-docker image ls
+#docker image ls
 
 docker build -t dockercelepuk/run run --progress=plain --no-cache
+
+docker build -t dockercelepuk/command command --progress=plain --no-cache
+
+#docker image inspect dockercelepuk/command
